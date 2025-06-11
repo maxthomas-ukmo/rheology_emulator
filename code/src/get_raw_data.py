0235b3df278605b8f8a8f9fc73f4ac71b7934ea5
Max Thomas <max.thomas@metoffice.gov.uk>
2025-06-11 15:09:41 +0100

    Added note that this isn't functional.

--- code/src/get_raw_data.py
+++ code/src/get_raw_data.py
@@ -2,17 +2,20 @@
 import xarray as xr
 import sys
 import dask
+import numpy as np
 
-def get_raw_data(get_path, potential_inputs=None):
+def get_raw_data(get_path, inputs=None):
 
-    if potential_inputs is None:
-        potential_inputs = ['sithic', 'sivolu', 'siconc', 'sivpnd', 'sivelu', 'sivelv', 'sivelo', 'utau_ai', 'vtau_ai', 'utau_oi', 'vtau_oi', 'sidive', 'sishea', 'sistre', 'normstr', 'sheastr']
+    if inputs is None:
+        inputs = ['sithic', 'sivolu', 'siconc', 'sivpnd', 'sivelu', 'sivelv', 'sivelo', 'utau_ai', 'vtau_ai', 'utau_oi', 'vtau_oi', 'sidive', 'sishea', 'sistre', 'normstr', 'sheastr']
 
     print(f"Retrieving raw data from {get_path}")
 
     raw = xr.open_dataset(get_path)
 
-    return raw[potential_inputs]
+    print(np.nanmax(raw['siconc']))
+
+    return raw[inputs]
 
 
 def save_raw_data(output_path, raw_data):
@@ -21,22 +24,28 @@ def save_raw_data(output_path, raw_data):
 
     raw_data = raw_data.chunk({'time_counter': -1})
 
+    print(np.nanmax(raw_data['siconc']))
+
     raw_data.to_netcdf(output_path, compute=False)
 
+    print(np.nanmax(raw_data['siconc']))
+
+    print('NOTE: This isnt working yet. Probably all of the raw getting needs to be submitted with qsub and loads of memory.')
+    
     dask.compute(raw_data)
 
 
-def get_and_save(get_path, output_path, potential_inputs=None):
-    raw_data = get_raw_data(get_path, potential_inputs)
+def get_and_save(get_path, output_path, inputs=None):
+    raw_data = get_raw_data(get_path, inputs)
     save_raw_data(output_path, raw_data)
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: python get_training_data.py <get_path> <output_path> [potential_inputs]")
+        print("Usage: python get_training_data.py <get_path> <output_path> [inputs]")
         sys.exit(1)
 
     get_path = sys.argv[1]
     output_path = sys.argv[2]
-    potential_inputs = sys.argv[3:] if len(sys.argv) > 3 else None
+    inputs = sys.argv[3:] if len(sys.argv) > 3 else None
 
-    get_and_save(get_path, output_path, potential_inputs)
+    get_and_save(get_path, output_path, inputs)
