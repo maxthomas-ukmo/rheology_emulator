cc1ee9727b84ee99bea3949a0f49a58cb00f8045
Max Thomas <max.thomas@metoffice.gov.uk>
2025-06-20 16:53:20 +0100

    Addedmultiple labels to pairs.

--- code/get_data.sh
+++ code/get_data.sh
@@ -1,2 +1,2 @@
-python -m main --get_data --interim_path ../data/intermediate/dq557_1ts_arctic.nc --pairs_path ../data/processed/pd_test.pkl
+python -m main --get_data --interim_path ../data/intermediate/dq557_1ts_arctic.nc --pairs_path ../data/processed/dq557_1ts_arctic.pkl --labels sivelv sivelu
 
