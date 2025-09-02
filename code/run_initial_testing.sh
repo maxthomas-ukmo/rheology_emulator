ce1597d5e35d3050ec8be2b49e5cd6dcb7b89c10
Max Thomas <max.thomas@metoffice.gov.uk>
2025-09-02 14:26:58 +0100

    Updated to zarr configs.

--- code/run_initial_testing.sh
+++ code/run_initial_testing.sh
@@ -1,5 +1,6 @@
 # loop over 1 to 7 and run main for each nn_it_x
 for i in {1..7}
 do
-  python -m main --train --training_cfg initial_testing/nn-it-$i
+  #python -m main --train --training_cfg initial_testing/nn-it-$i
+  python -m main --train --training_cfg initial_testing_12day/nn-it-$i
 done
